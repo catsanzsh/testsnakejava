0d1b78079592cfea3b6db344c32ed60091a7100a
catsanzsh <84363801+catsanzsh@users.noreply.github.com>
2024-09-30 15:04:00 -0700

    Create utrasnakejdk.java

---
+++ utrasnakejdk.java
@@ -0,0 +1,14 @@
+# Java Snake Game
+
+This repository contains the source code for a simple Snake Game implemented in Java. It's a classic game where the player maneuvers a line which grows in length, with the line itself being a primary obstacle.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+What things you need to install the software and how to install them:
+
+```bash
+Java JDK 8 or later
